49c53ee1763d1872a60805e20d0b2ff3e9657765
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-05-16 14:58:33 +0300

    hw9 first commit

---
+++ src/main/java/pro/sky/profjd5_0envhw9/ProfJd50EnvHw9Application.java
@@ -0,0 +1,13 @@
+package pro.sky.profjd5_0envhw9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfJd50EnvHw9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfJd50EnvHw9Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/profjd5_0envhw9/ProfJd50EnvHw9ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.profjd5_0envhw9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfJd50EnvHw9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
